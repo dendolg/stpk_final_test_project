f50ff312e62bd9830d091fbd4a2190f5a4d2dda4
dendolg <dendolgov1212@gmail.com>
2025-10-24 16:32:47 +0300

    added is_element_present method to the base_page, should_be_login_link to the main_page, added login link test to the test_main_page

--- pages/base_page.py
+++ pages/base_page.py
@@ -1,7 +1,19 @@
+from selenium.common.exceptions import NoSuchElementException
+
+
 class BasePage:
-    def __init__(self, browser, url):
+    def __init__(self, browser, url, timeout=10):
         self.browser = browser
         self.url = url
+        self.browser.implicitly_wait(timeout)
 
     def open(self):
         self.browser.get(self.url)
+
+    def is_element_present(self, how_to_find, locator_to_find):
+        try:
+            self.browser.find_element(how_to_find, locator_to_find)
+        except NoSuchElementException:
+            return False
+        else:
+            return True
