0737b341fe032726b2ecccefd347c10cee872275
dendolg <dendolgov1212@gmail.com>
2025-10-25 15:50:01 +0300

    made locators for login page in locators.py, added 'should be' methods for LoginPage class

--- pages/locators.py
+++ pages/locators.py
@@ -3,3 +3,14 @@ from selenium.webdriver.common.by import By
 
 class MainPageLocators:
     LOGIN_LINK = (By.CSS_SELECTOR, "#login_link")
+
+
+class LoginPageLocators:
+    LOGIN_EMAIL = (By.NAME, "login-username_inv")
+    LOGIN_PASSWORD = (By.NAME, "login-password_inv")
+    LOGIN_FORGOT_PASSWORD_LINK = (By.PARTIAL_LINK_TEXT, "password-reset_inv")
+    LOGIN_SUBMIT_BUTTON = (By.NAME, "login_submit_inv")
+    REG_EMAIL = (By.NAME, "registration-email_inv")
+    REG_PASSWORD = (By.NAME, "registration-password1_inv")
+    REG_PASSWORD_CONFIRM = (By.NAME, "registration-password2_inv")
+    REG_SUBMIT_BUTTON = (By.NAME, "registration_submit_inv")
--- pages/login_page.py
+++ pages/login_page.py
@@ -1,4 +1,5 @@
 from .base_page import BasePage
+from .locators import LoginPageLocators
 
 
 class LoginPage(BasePage):
@@ -8,13 +9,20 @@ class LoginPage(BasePage):
         self.should_be_register_form()
 
     def should_be_login_url(self):
-        # реализуйте проверку на корректный url адрес
-        assert True
+        assert "login" in self.browser.current_url, "There is no 'login' in page url"
 
     def should_be_login_form(self):
-        # реализуйте проверку, что есть форма логина
-        assert True
+        assert (
+            self.is_element_present(*LoginPageLocators.LOGIN_EMAIL)
+            * self.is_element_present(*LoginPageLocators.LOGIN_PASSWORD)
+            * self.is_element_present(*LoginPageLocators.LOGIN_FORGOT_PASSWORD_LINK)
+            * self.is_element_present(*LoginPageLocators.LOGIN_SUBMIT_BUTTON)
+        ), "One or few login form elements wasn't found"
 
     def should_be_register_form(self):
-        # реализуйте проверку, что есть форма регистрации на странице
-        assert True
+        assert (
+            self.is_element_present(*LoginPageLocators.REG_EMAIL)
+            * self.is_element_present(*LoginPageLocators.REG_PASSWORD)
+            * self.is_element_present(*LoginPageLocators.REG_PASSWORD_CONFIRM)
+            * self.is_element_present(*LoginPageLocators.REG_SUBMIT_BUTTON)
+        ), "One or few register form elements wasn't found"
